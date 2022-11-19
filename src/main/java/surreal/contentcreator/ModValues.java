ae97dd187e668f4ba76adc16a17947589d401aba
ISurrealI <merhabalarexoria@gmail.com>
2022-11-19 23:43:40 +0300

    Not alpha

--- src/main/java/surreal/contentcreator/ModValues.java
+++ src/main/java/surreal/contentcreator/ModValues.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Loader;
 public class ModValues {
     public static final String MODID = "contentcreator";
     public static final String NAME = "Content Creator";
-    public static final String VERSION = "1.3.6-2a";
+    public static final String VERSION = "1.3.6-2";
 
     public static final boolean AVARITIA = Loader.isModLoaded("avaritia");
 }
